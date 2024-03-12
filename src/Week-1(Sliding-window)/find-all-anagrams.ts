35beff7d7ef7f6033c9f9c900766a261d7563022
Nina <labferment10@gmail.com>
2024-03-12 16:50:13 +0100

    update

---
+++ src/Week-1(Sliding-window)/find-all-anagrams.ts
@@ -0,0 +1,7 @@
+// 438. Find All Anagrams in a String
+
+// Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+function findAnagrams(s: string, p: string): number[] {}
--- src/Week-1(Sliding-window)/maximum-average-subarray.ts
+++ src/Week-1(Sliding-window)/maximum-average-subarray.ts
@@ -4,6 +4,19 @@
 
 // Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
 
+// EXAMPLES:
+
+// Example 1
+
+// Input: nums = [1,12,-5,-6,50,3], k = 4
+// Output: 12.75000
+// Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+
+// Example 2
+
+// Input: nums = [5], k = 1;
+// Output: 5.0;
+
 function findMaxAverage(nums: number[], k: number): number {
   let sum: number = 0;
   let maxSum: number = -Infinity;
